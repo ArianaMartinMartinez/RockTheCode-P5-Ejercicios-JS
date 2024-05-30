f7b2a558b0d78abc471a8d6e1a155557ffc85978
Ariana Martin <arianamartinmartinez@gmail.com>
2024-05-30 23:44:31 +0200

    Ejercicio 24 hecho

--- Ejercicios/ejercicio_24.js
+++ Ejercicios/ejercicio_24.js
@@ -1,6 +1,6 @@
 /*
 Usa un bucle para sumar el total de las ventas (sellCount) de todos los productos.
-
+*/
 
 const products = [
   { name: "Funko Dr. Strange", sellCount: 10 },
@@ -8,4 +8,10 @@ const products = [
   { name: "Sable laser FX", sellCount: 23 },
   { name: "Varita de Voldemort", sellCount: 6 },
 ];
-*/
+
+let sum = 0;
+for(const product of products) {
+  sum += product.sellCount;
+}
+
+console.log('Total de las ventas de los productos:', sum);
