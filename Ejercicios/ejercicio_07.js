dcfdd9059bc2484e65f969e8aa1dec2e7eacc2e4
Ariana Martin <arianamartinmartinez@gmail.com>
2024-05-30 16:40:58 +0200

    Ejercicio 7 hecho

--- Ejercicios/ejercicio_07.js
+++ Ejercicios/ejercicio_07.js
@@ -1,7 +1,11 @@
-/*
-Completa la función que tomando dos números como argumento devuelva el más alto.
+//Completa la función que tomando dos números como argumento devuelva el más alto.
 
 function sum(numberOne , numberTwo) {
-  // insert code
+  if(numberOne > numberTwo) {
+    console.log('El número mayor es', numberOne);
+  } else {
+    console.log('El número mayor es', numberTwo);
+  }
 }
-*/
+
+sum(10, 2);
