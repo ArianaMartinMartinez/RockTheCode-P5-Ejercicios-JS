e21711c95c92431b2f43f53faf10e8061f0a0ba5
Ariana Martin <arianamartinmartinez@gmail.com>
2024-05-30 16:47:39 +0200

    Ejercicio 8 hecho

--- Ejercicios/ejercicio_08.js
+++ Ejercicios/ejercicio_08.js
@@ -1,10 +1,22 @@
 /*
-Buscar la palabra más larga Completa la función que tomando un array de strings como argumento devuelva el más largo, en caso de que dos strings tenga la misma longitud deberá devolver el primero.
+Buscar la palabra más larga 
 
-Puedes usar este array para probar tu función:
+Completa la función que tomando un array de strings como argumento devuelva 
+el más largo, en caso de que dos strings tenga la misma longitud deberá devolver el primero.
+
+Puedes usar este array para probar tu función:*/
 
 const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
 function findLongestWord(param) {
-  // insert code
+  let longestWord = param[0];
+
+  for(let i=0; i< param.length; i++) {
+    if(avengers[i].length > longestWord.length) {
+      longestWord = avengers[i];
+    }
+  }
+
+  return longestWord;
 }
-*/
+
+console.log('La palabra más larga es:', findLongestWord(avengers));
