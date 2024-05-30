f4adfa4445656a5c9f4db14e76d7929d44cd13c2
Ariana Martin <arianamartinmartinez@gmail.com>
2024-05-30 22:52:39 +0200

    Ejercicio 20 hecho

--- Ejercicios/ejercicio_20.js
+++ Ejercicios/ejercicio_20.js
@@ -1,7 +1,14 @@
 /*
-For...of avanzado: Usa un bucle for...of para recorrer todos los juguetes y añade los que tengan más de 15 ventas (sellCount) al array popularToys. Imprimelo por consola.. Puedes usar este array:
+For...of avanzado: 
 
+Usa un bucle for...of para recorrer todos los juguetes y añade los que 
+tengan más de 15 ventas (sellCount) al array popularToys. 
+Imprimelo por consola.. 
+
+Puedes usar este array:
+*/
 const popularToys = [];
+
 const toys = [
 	{id: 5, name: 'Buzz MyYear', sellCount: 10}, 
 	{id: 11, name: 'Action Woman', sellCount: 24}, 
@@ -9,4 +16,11 @@ const toys = [
 	{id: 40, name: 'El gato con Guantes', sellCount: 8},
 	{id: 40, name: 'El gato felix', sellCount: 35}
 ]
-*/
+
+for(const toy of toys) {
+	if(toy.sellCount > 15) {
+		popularToys.push(toy);
+	}
+}
+
+console.log('Juguetes populares:', popularToys);
