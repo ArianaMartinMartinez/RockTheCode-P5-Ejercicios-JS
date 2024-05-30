3fa9dff19800b8e5d1395bb0de2c7f1e3e120c08
Ariana Martin <arianamartinmartinez@gmail.com>
2024-05-30 23:41:12 +0200

    Ejercicio 23 hecho

--- Ejercicios/ejercicio_23.js
+++ Ejercicios/ejercicio_23.js
@@ -1,6 +1,11 @@
 /*
-Usa un bucle para crear 3 arrays de peliculas filtrados por categorias. Pelicula pequeña, menos de 100 minutos, pelicula mediana, mas de 100 minutos y menos de 200 y pelicula grande, mas de 200 minutos. Imprime cada array en por consola.
+Usa un bucle para crear 3 arrays de peliculas filtrados por categorias. 
 
+Pelicula pequeña, menos de 100 minutos, pelicula mediana, mas de 100 minutos 
+y menos de 200 y pelicula grande, mas de 200 minutos. 
+
+Imprime cada array en por consola.
+*/
 const movies = [
   { name: "Titan A.E.", durationInMinutes: 130 },
   { name: "Nightmare before Christmas", durationInMinutes: 225 },
@@ -9,4 +14,21 @@ const movies = [
   { name: "Star Wars: A New Hope", durationInMinutes: 214 },
   { name: "Terminator", durationInMinutes: 140 },
 ];
-*/
+
+const smallMovies = [];
+const mediumMovies = [];
+const largeMovies = [];
+
+for(const movie of movies) {
+  if(movie.durationInMinutes < 100) {
+    smallMovies.push(movie);
+  } else if(movie.durationInMinutes >= 100 && movie.durationInMinutes <= 200) {
+    mediumMovies.push(movie);
+  } else {
+    largeMovies.push(movie);
+  }
+}
+
+console.log('Películas pequeñas:', smallMovies);
+console.log('Películas medianas:', mediumMovies);
+console.log('Películas grandes:', largeMovies);
